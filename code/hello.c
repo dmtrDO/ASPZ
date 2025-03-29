c96115d8b5b34760a035d825a78fc9a4c24b4661
dmtrDO <84675940+dmtrDO@users.noreply.github.com>
2025-03-29 12:52:43 +0200

    Delete code directory

--- code/hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int arr[1000];
-int main(void) {
-	printf("hello world\n"); 
-	return 0;
-}
--- code/task1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> 
-#include <time.h>
-int main(void) {
-	printf("Size of time_t is %zu\n", sizeof(time_t)); 
-	return 0;
-}
